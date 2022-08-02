6f98cbcf421fe112516b189888d854d444fdba3b
wleksono <108651008+wleksono@users.noreply.github.com>
2022-08-02 11:40:19 +1000

    Update InstallAvailableUpdates.ps1

--- InstallAvailableUpdates.ps1
+++ InstallAvailableUpdates.ps1
@@ -17,32 +17,32 @@ $SearchResult = $UpdateSearcher.Search($Criteria).Updates
 if($SearchResult.count -ne 0){
     foreach ($entry in $SearchResult){
 	    if ($entry.isDownloaded -eq $false){		
-		    $updateSession = New-Object -ComObject 'Microsoft.Update.Session'
-		    $updatesToDownload = New-Object -ComObject 'Microsoft.Update.UpdateColl'
-		    $updatesToDownload.Add($entry) | Out-Null
+		$updateSession = New-Object -ComObject 'Microsoft.Update.Session'
+		$updatesToDownload = New-Object -ComObject 'Microsoft.Update.UpdateColl'
+		$updatesToDownload.Add($entry) | Out-Null
         	$downloader = $updateSession.CreateUpdateDownloader()
         	$downloader.Updates = $updatesToDownload
         	$downloadResult = $downloader.Download()
-		    if ($downloadResult.ResultCode -eq 2) {
-			    $updatesToInstall = New-Object -ComObject 'Microsoft.Update.UpdateColl'
-        		$updatesToInstall.Add($entry) | Out-Null 
-
-        		$installer = New-Object -ComObject 'Microsoft.Update.Installer'
-        		$installer.Updates = $updatesToInstall        
-        		$installResult = $installer.Install()
+		if ($downloadResult.ResultCode -eq 2) {
+			$updatesToInstall = New-Object -ComObject 'Microsoft.Update.UpdateColl'
+			$updatesToInstall.Add($entry) | Out-Null
+			
+			$installer = New-Object -ComObject 'Microsoft.Update.Installer'
+			$installer.Updates = $updatesToInstall        
+			$installResult = $installer.Install()
 				
-				Clear-Variable updatesToInstall -Force -ErrorAction SilentlyContinue
+			Clear-Variable updatesToInstall -Force -ErrorAction SilentlyContinue
 		    }
 		Clear-Variable updatesToDownload -Force -ErrorAction SilentlyContinue
 	    }
 	    else{
-		    $updatesToInstall = New-Object -ComObject 'Microsoft.Update.UpdateColl'
-		    $updatesToInstall.Add($entry) | Out-Null 
+		$updatesToInstall = New-Object -ComObject 'Microsoft.Update.UpdateColl'
+		$updatesToInstall.Add($entry) | Out-Null 
 
         	$installer = New-Object -ComObject 'Microsoft.Update.Installer'
         	$installer.Updates = $updatesToInstall        
         	$installResult = $installer.Install()
-		    Clear-Variable updatesToInstall -Force -ErrorAction SilentlyContinue
+		Clear-Variable updatesToInstall -Force -ErrorAction SilentlyContinue
 	    }
     }
     $Sysinfo = New-Object -ComObject Microsoft.Update.SystemInfo
