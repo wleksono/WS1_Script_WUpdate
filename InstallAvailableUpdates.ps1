15aeefa25036fb1eeaa59149cfa7ce5f24a20f4a
wleksono <108651008+wleksono@users.noreply.github.com>
2022-10-07 18:41:18 +1100

    Update InstallAvailableUpdates.ps1

--- InstallAvailableUpdates.ps1
+++ InstallAvailableUpdates.ps1
@@ -1,55 +1,81 @@
 $Sysinfo = New-Object -ComObject Microsoft.Update.SystemInfo
 $pending = $Sysinfo.RebootRequired
-if ($pending){ 
-	shutdown.exe /r /f /t 120
-	exit 55
+if ($pending){
+	try{
+	    shutdown.exe /r /f /t 120
+	}
+	catch{
+	    exit 1
+	}
+	exit 2
 }
-
-$testnet = Test-NetConnection -ComputerName www.catalog.update.microsoft.com -CommonTCPPort HTTP
-if($testnet.TcpTestSucceeded -eq "True"){}Else{exit 44}
 
 $Session = [activator]::CreateInstance([type]::GetTypeFromProgID("Microsoft.Update.Session"))#,$Computer))
 $UpdateSearcher = $Session.CreateUpdateSearcher()
 
 $Criteria = "IsHidden=0 and IsInstalled=0 and IsAssigned=1"
-$SearchResult = $UpdateSearcher.Search($Criteria).Updates
+
+try{
+    $SearchResult = $UpdateSearcher.Search($Criteria).Updates
+}
+catch{
+    exit 1
+}
 
 if($SearchResult.count -ne 0){
     foreach ($entry in $SearchResult){
 	    if ($entry.isDownloaded -eq $false){		
-		$updateSession = New-Object -ComObject 'Microsoft.Update.Session'
-		$updatesToDownload = New-Object -ComObject 'Microsoft.Update.UpdateColl'
-		$updatesToDownload.Add($entry) | Out-Null
-        	$downloader = $updateSession.CreateUpdateDownloader()
-        	$downloader.Updates = $updatesToDownload
-        	$downloadResult = $downloader.Download()
-		if ($downloadResult.ResultCode -eq 2) {
-			$updatesToInstall = New-Object -ComObject 'Microsoft.Update.UpdateColl'
-			$updatesToInstall.Add($entry) | Out-Null
-			
-			$installer = New-Object -ComObject 'Microsoft.Update.Installer'
-			$installer.Updates = $updatesToInstall        
-			$installResult = $installer.Install()
-				
-			Clear-Variable updatesToInstall -Force -ErrorAction SilentlyContinue
+		    $updateSession = New-Object -ComObject 'Microsoft.Update.Session'
+		    $updatesToDownload = New-Object -ComObject 'Microsoft.Update.UpdateColl'
+		    $updatesToDownload.Add($entry) | Out-Null
+            $downloader = $updateSession.CreateUpdateDownloader()
+            $downloader.Updates = $updatesToDownload
+		    try{
+        	    $downloadResult = $downloader.Download()
 		    }
-		Clear-Variable updatesToDownload -Force -ErrorAction SilentlyContinue
+		    catch{
+		        "Download failed"
+		    }
+		    if ($downloadResult.ResultCode -eq 2) {
+			    $updatesToInstall = New-Object -ComObject 'Microsoft.Update.UpdateColl'
+			    $updatesToInstall.Add($entry) | Out-Null
+			
+			    $installer = New-Object -ComObject 'Microsoft.Update.Installer'
+			    $installer.Updates = $updatesToInstall        
+			    try{
+			        $installResult = $installer.Install()
+			    }
+			    catch{
+                    "Install Failed"
+			    }
+			    Clear-Variable updatesToInstall -Force -ErrorAction SilentlyContinue
+		    }
+		    Clear-Variable updatesToDownload -Force -ErrorAction SilentlyContinue
 	    }
 	    else{
-		$updatesToInstall = New-Object -ComObject 'Microsoft.Update.UpdateColl'
-		$updatesToInstall.Add($entry) | Out-Null 
+		    $updatesToInstall = New-Object -ComObject 'Microsoft.Update.UpdateColl'
+		    $updatesToInstall.Add($entry) | Out-Null 
 
-        	$installer = New-Object -ComObject 'Microsoft.Update.Installer'
-        	$installer.Updates = $updatesToInstall        
-        	$installResult = $installer.Install()
-		Clear-Variable updatesToInstall -Force -ErrorAction SilentlyContinue
+            $installer = New-Object -ComObject 'Microsoft.Update.Installer'
+            $installer.Updates = $updatesToInstall        
+		    try{
+        	    $installResult = $installer.Install()
+		    }
+		    catch{
+                "Install Failed"
+		    }
+		    Clear-Variable updatesToInstall -Force -ErrorAction SilentlyContinue
 	    }
     }
     $Sysinfo = New-Object -ComObject Microsoft.Update.SystemInfo
     $pending = $Sysinfo.RebootRequired
-    if ($pending) { shutdown.exe /r /f /t 120 }
-    exit
+    if ($pending) {
+        try{
+	        shutdown.exe /r /f /t 120
+        }
+        catch{
+            exit 1
+        }
+    }
 }
-else{
-    exit
-}
+exit
